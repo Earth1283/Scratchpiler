45282663ab36a90a6a18a84c74798da811cc2081
Earth1283 <joseph_cio@hotmail.com>
2026-06-19 19:58:26 +0800

    v1.0

--- scratchpiler.user.js
+++ scratchpiler.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Scratchpiler
 // @namespace    https://scratch.mit.edu/
-// @version      0.2.0
+// @version      1.0
+// @author       Earth1283
 // @description  Monaco-powered DSL compiler and editor overlay for Scratch
 // @match        https://scratch.mit.edu/projects/editor/*
 // @match        https://scratch.mit.edu/projects/*/editor*
